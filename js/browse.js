0fa50700769a53c9bac7caa2c02495f382c3efba
XY Feng <fxy0111@gmail.com>
2014-07-21 17:22:41 -0400

    push to server

--- js/browse.js
+++ js/browse.js
@@ -1,6 +1,6 @@
 (function() {
   $(document).ready(function() {
-    var bounds, center, countryCode, countryID, countryLayer, countryName, feature, featureClicked, getStyle, global_projection, highlightFeature, list, map, mapID, onEachFeature, one, popup, region_height, region_path, region_projection, region_width, resetFeature, scale, svg, topLayer, topPane, _i, _len, _ref;
+    var char, closeTooltip, column, columns, country, countryLayer, country_id, country_list, feature, featureClicked, first_letter, getStyle, highlightFeature, map, mapID, onEachFeature, one_char_box, one_char_labe, one_column, popup, resetFeature, topLayer, topPane, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _results;
     mapID = 'xyfeng.ijpo6lio';
     getStyle = function(feature) {
       return {
@@ -9,6 +9,9 @@
         fillColor: '#fff'
       };
     };
+    closeTooltip = window.setTimeout(function() {
+      return map.closePopup();
+    }, 100);
     highlightFeature = function(e) {
       var countryID, layer;
       layer = e.target;
@@ -18,8 +21,14 @@
         opacity: 0.2,
         color: '#ccc',
         fillOpacity: 1.0,
-        fillColor: '#fff'
+        fillColor: '#f5837b'
       });
+      popup.setLatLng(e.latlng);
+      popup.setContent("<div class='marker-title'>" + layer.feature.properties.name + "</div>" + layer.feature.properties.value + " datasets");
+      if (!popup._map) {
+        popup.openOn(map);
+      }
+      window.clearTimeout(closeTooltip);
     };
     resetFeature = function(e) {
       var layer;
@@ -29,6 +38,9 @@
         fillOpacity: 1.0,
         fillColor: '#fff'
       });
+      closeTooltip = window.setTimeout(function() {
+        return map.closePopup();
+      }, 100);
     };
     featureClicked = function(e) {
       var countryName, layer;
@@ -43,6 +55,24 @@
         click: featureClicked
       });
     };
+    _ref = worldJSON['features'];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      feature = _ref[_i];
+      country_id = feature.id;
+      first_letter = country_id.substring(0, 1);
+      feature.properties.value = 0;
+      if (countries[first_letter]) {
+        _ref1 = countries[first_letter];
+        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+          country = _ref1[_j];
+          if (country[0] === country_id) {
+            feature.properties.value = country[2];
+            console.log(country[2]);
+            break;
+          }
+        }
+      }
+    }
     map = L.mapbox.map('map', mapID, {
       center: [20, 10],
       zoom: 2,
@@ -53,6 +83,7 @@
         noWrap: true
       }
     });
+    map.scrollWheelZoom.disable();
     map.featureLayer.setFilter(function() {
       return false;
     });
@@ -69,31 +100,38 @@
     topLayer.addTo(map);
     topPane.appendChild(topLayer.getContainer());
     topLayer.setZIndex(7);
-    console.log(map);
-    global_projection = d3.geo.mercator();
-    region_path = d3.geo.path().projection(global_projection);
-    region_width = 80;
-    region_height = 60;
-    _ref = worldJSON.features;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      feature = _ref[_i];
-      countryName = feature.properties.name;
-      countryID = feature.id;
-      countryCode = feature.code;
-      if (countryCode) {
-        countryCode = countryCode.toLowerCase();
-      }
-      list = $('#country_list');
-      one = $('<div class="col-md-3 country-box" id="region_'+ countryID +'"></div>').appendTo(list);
-      svg = d3.select("#region_" + countryID).append('svg').attr('width', region_width).attr('height', region_height);
-      bounds = region_path.bounds(feature);
-      scale = global_projection.scale() * d3.min([region_width / (bounds[1][0] - bounds[0][0]), region_height / (bounds[1][1] - bounds[0][1])]) * 0.75;
-      center = global_projection.invert(region_path.centroid(feature));
-      region_projection = d3.geo.mercator().center(center).scale(scale).translate([region_width / 2, region_height / 2]);
-      svg.append('path').datum(feature).attr('class', 'region-path').attr('d', d3.geo.path().projection(region_projection));
-      $('<a>' + countryName + '</a>').appendTo(one);
-      $('<label class="region-flag"><span class="flag-icon flag-icon-'+ countryCode +'"></span></label>').appendTo(one);
+    columns = [['A', 'B', 'C'], ['D', 'E', 'F', 'G', 'H'], ['I', 'J', 'K', 'L'], ['M', 'N', 'O', 'P'], ['Q', 'R', 'S', 'T'], ['U', 'V', 'Y', 'Z']];
+    country_list = $('#country_list');
+    _results = [];
+    for (_k = 0, _len2 = columns.length; _k < _len2; _k++) {
+      column = columns[_k];
+      one_column = $('<div class="col-md-2"></div>').appendTo(country_list);
+      _results.push((function() {
+        var _l, _len3, _results1;
+        _results1 = [];
+        for (_l = 0, _len3 = column.length; _l < _len3; _l++) {
+          char = column[_l];
+          one_char_box = $("<div class='char-box'></div>").appendTo(one_column);
+          one_char_labe = $("<div class='char-label'>" + char + "</div>").appendTo(one_char_box);
+          _results1.push((function() {
+            var _len4, _m, _ref2, _results2;
+            _ref2 = countries[char];
+            _results2 = [];
+            for (_m = 0, _len4 = _ref2.length; _m < _len4; _m++) {
+              country = _ref2[_m];
+              if (country[2] === 0) {
+                _results2.push($("<div class='country-item inactive'>" + country[1] + "</div>").appendTo(one_char_box));
+              } else {
+                _results2.push($("<div class='country-item'>" + country[1] + "</div>").appendTo(one_char_box));
+              }
+            }
+            return _results2;
+          })());
+        }
+        return _results1;
+      })());
     }
+    return _results;
   });
 
 }).call(this);
